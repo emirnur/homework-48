078f9b824d12b5f9fca7a2826c754379e55b3aaf
emirnur <nur.emirlan@gmail.com>
2019-09-24 00:57:25 +0600

    add home page

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from webapp.models import Product
 
-# Create your views here.
+
+def index_view(request):
+
+    products = Product.objects.filter(amount__gt=0).order_by('category', 'name')
+    return render(request, 'index.html', context={
+        'products': products
+    })
+
+
