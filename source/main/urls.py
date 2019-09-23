f546f6e9afb30e100d7d475eb374e0557aea0379
emirnur <nur.emirlan@gmail.com>
2019-09-24 01:38:38 +0600

    add function of search

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import index_view, product_detail_view
+from webapp.views import index_view, product_detail_view, search_by_name
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view, name='index'),
-    path('products/<int:pk>/', product_detail_view, name='product_detail')
+    path('products/<int:pk>/', product_detail_view, name='product_detail'),
+    path('search/', search_by_name, name='search_by_name')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from webapp.models import Product
+from webapp.forms import SearchForm
 
 
 def index_view(request):
-
     products = Product.objects.filter(amount__gt=0).order_by('category', 'name')
+    search_form = SearchForm()
     return render(request, 'index.html', context={
-        'products': products
+        'products': products,
+        'search_form': search_form
     })
 
 def product_detail_view(request, pk):
@@ -15,5 +17,14 @@ def product_detail_view(request, pk):
     return render(request, 'product.html', context={
         'product': product
     })
+
+def search_by_name(request, *args, **kwargs):
+    search = request.GET.get('search')
+    products = Product.objects.filter(name__contains=search)
+    form = SearchForm()
+    return render(request, 'index.html', context={
+        'products': products,
+        'form': form,
+})
 
 
